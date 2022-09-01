d9bd34bc2995bbdc1de1f92c26670f20d660f2b7
todabbb <toda.0206@icloud.com>
2022-09-01 09:15:30 +0000

    21error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_19_023748) do
+ActiveRecord::Schema.define(version: 2022_09_01_020739) do
 
   create_table "categories", force: :cascade do |t|
     t.string "major_category_name"
@@ -68,6 +68,16 @@ ActiveRecord::Schema.define(version: 2022_08_19_023748) do
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "shopping_cart_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shopping_carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
