803dbab1fd0d41360fc9e0cd92c6f587985a44f9
todabbb <toda.0206@icloud.com>
2022-09-06 10:07:36 +0000

    dashboard_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "dashboard", :to => "dashboard#index"
+  
   get 'shopping_carts/index'
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
