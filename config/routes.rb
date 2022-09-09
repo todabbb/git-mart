7b9847aeaaa2c736dfbe9e0c1754f105e4f8b7d9
todabbb <toda.0206@icloud.com>
2022-09-09 10:10:41 +0000

    dashboard_5

--- config/routes.rb
+++ config/routes.rb
@@ -1,55 +1,56 @@
 Rails.application.routes.draw do
-  
   devise_for :admins, :controllers => {
-     :sessions => 'admins/sessions'
-   }
- 
-   devise_scope :admin do
-     get "dashboard", :to => "dashboard#index"
-     get "dashboard/login", :to => "admins/sessions#new"
-     post "dashboard/login", :to => "admins/sessions#create"
-     delete "dashboard/logout", :to => "admins/sessions#destroy"
-   end
-   
-   namespace :dashboard do
-     resources :categories, except: [:new]
-     resources :products, except: [:show]
-   end
-  
-  get 'shopping_carts/index'
-  devise_for :users, :controllers => {
-  :registrations => 'users/registrations',
-  :sessions => 'users/sessions',
-  :passwords => 'users/passwords',
-  :confirmations => 'users/confirmations',
-  :unlocks => 'users/unlocks',
-}
+    :sessions => 'admins/sessions',
+  }
 
-devise_scope :user do
-   root :to => "web#index"
-  get "signup", :to => "users/registrations#new"
-  get "verify", :to => "users/registrations#verify"
-  get "login", :to => "users/sessions#new"
-  delete "logout", :to => "users/sessions#destroy"
-end
-
-resource :users, only: [:edit, :update] do
-  collection do
-    get "cart", :to => "shopping_carts#index"
-    post "cart/create", :to => "shopping_carts#create"
-    delete "cart", :to => "shopping_carts#destroy"
-    get "mypage", :to => "users#mypage"
-    get "mypage/edit", :to => "users#edit"
-    get "mypage/address/edit", :to => "users#edit_address"
-    put "mypage", :to => "users#update"
-    get "mypage/edit_password", :to =>"users#edit_password"
-    put "mypage/password", :to => "users#update_password"
-    get  "mypage/favorite", :to => "users#favorite"
+  devise_scope :admin do
+    get "dashboard", :to => "dashboard#index"
+    get "dashboard/login", :to => "admins/sessions#new"
+    post "dashboard/login", :to => "admins/sessions#create"
+    delete "dashboard/logout", :to => "admins/sessions#destroy"
   end
-end
+
+  namespace :dashboard do
+     resources :major_categories, except: [:new]
+    resources :categories, except: [:new]
+    resources :products, except: [:show]
+  end
+
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+  }
+
+  devise_scope :user do
+    root :to => "web#index"
+    get "signup", :to => "users/registrations#new"
+    get "verify", :to => "users/registrations#verify"
+    get "login", :to => "users/sessions#new"
+    delete "logout", :to => "users/sessions#destroy"
+  end
+
+  resource :users, only: [:edit, :update] do
+    collection do
+      get "cart", :to => "shopping_carts#index"
+      post "cart/create", :to => "shopping_carts#create"
+      delete "cart", :to => "shopping_carts#destroy"
+      get "mypage", :to => "users#mypage"
+      get "mypage/edit", :to => "users#edit"
+      get "mypage/address/edit", :to => "users#edit_address"
+      put "mypage", :to => "users#update"
+      get "mypage/edit_password", :to =>"users#edit_password"
+      put "mypage/password", :to => "users#update_password"
+      get "mypage/favorite", :to => "users#favorite"
+    end
+  end
+
   resources :products do
     member do
-      get :favorite
+      post :favorite
     end
     resources :reviews, only: [:create]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_06_100924) do
+ActiveRecord::Schema.define(version: 2022_09_09_095116) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 2022_09_06_100924) do
     t.datetime "created_at"
     t.index ["likeable_id", "likeable_type"], name: "fk_likeables"
     t.index ["liker_id", "liker_type"], name: "fk_likes"
+  end
+
+  create_table "major_categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "mentions", force: :cascade do |t|
